a674a4e5fcbc95803dcffd1e438b5a62cfdb980e
fabiohidekihirose <fabiohidekihirose@gmail.com>
2023-06-28 22:40:46 +0900

    Update: login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
+import DescriptionAuth from "@/components/DescriptionAuth";
+
 export default function Home() {
   return (
     <main className="flex max-lg:flex-col min-h-screen items-center bg-[#112D4E] text-[#112D4E]">
-      <div className="flex flex-col max-w-[500px] lg:ml-[120px] mx-[40px] max-lg:mt-[50px]">
-        <div className="flex items-center space-x-[10px] mb-[20px]">
-          <img src="/logo-login.png" className="w-[70px] md:w-[100px]"></img>
-          <p className="md:text-[40px] text-[25px] text-[#DBE2EF] font-[700]">
-            TRAVEL N SHARE
-          </p>
-        </div>
-        <p className="text-[#DBE2EF] text-[15px]">
-          A vibrant social network designed exclusively for adventurous
-          travelers to connect and exchange their remarkable journeys from
-          around the globe
-        </p>
-      </div>
-      <div className="border-[1px] max-lg:mt-[50px] p-6 m-auto rounded-[10px] bg-[#DBE2EF] shadow-[5px_10px_30px_rgb(63,114,175)] max-w-[400px]">
+      <DescriptionAuth />
+      <div className="border-[1px] max-lg:mt-[50px] p-10 m-auto rounded-[10px] bg-[#DBE2EF] shadow-[5px_10px_30px_rgb(63,114,175)] max-w-[400px]">
         <h1 className="font-[600] text-[40px]">Welcome Back!</h1>
         <h2 className="mb-[20px]">Please, enter your details to login</h2>
         <form className="flex flex-col">
---
+++ src/components/DescriptionAuth.tsx
@@ -0,0 +1,16 @@
+export default function DescriptionAuth() {
+  return (
+    <div className="flex flex-col max-w-[500px] lg:ml-[120px] mx-[40px] max-lg:mt-[50px]">
+      <div className="flex items-center space-x-[10px] mb-[20px]">
+        <img src="/logo/logo-login.png" className="w-[70px] md:w-[100px]"></img>
+        <p className="md:text-[40px] text-[25px] text-[#DBE2EF] font-[700]">
+          TRAVEL N SHARE
+        </p>
+      </div>
+      <p className="text-[#DBE2EF] text-[15px]">
+        A vibrant social network designed exclusively for adventurous travelers
+        to connect and exchange their remarkable journeys from around the globe
+      </p>
+    </div>
+  );
+}
