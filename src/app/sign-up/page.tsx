3c6167e88340bb2ba7b37f6a13bea33b1d687dea
fabiohidekihirose <fabiohidekihirose@gmail.com>
2023-06-29 23:20:11 +0900

    Update: add state for the form data

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,6 +1,20 @@
+"use client";
+
 import DescriptionAuth from "@/components/DescriptionAuth";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({
+    email: "",
+    full_name: "",
+    username: "",
+    password: "",
+  });
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="flex max-lg:flex-col min-h-screen items-center bg-[#112D4E] text-[#112D4E]">
       <DescriptionAuth />
@@ -9,13 +23,33 @@ export default function SignUp() {
         <h2 className="mb-[20px]">Create an account to use Travel N Share</h2>
         <form className="flex flex-col">
           <label>Email</label>
-          <input type="email" className="mb-[20px] rounded-[10px] p-2"></input>
-          <label>Name</label>
-          <input type="text" className="mb-[20px] rounded-[10px] p-2"></input>
+          <input
+            name="email"
+            type="email"
+            className="mb-[20px] rounded-[10px] p-2"
+            onChange={changeHandler}
+          ></input>
+          <label>Full Name</label>
+          <input
+            name="full_name"
+            type="text"
+            className="mb-[20px] rounded-[10px] p-2"
+            onChange={changeHandler}
+          ></input>
           <label>Username</label>
-          <input type="text" className="mb-[20px] rounded-[10px] p-2"></input>
+          <input
+            name="username"
+            type="text"
+            className="mb-[20px] rounded-[10px] p-2"
+            onChange={changeHandler}
+          ></input>
           <label>Password</label>
-          <input type="password" className="mb-[30px] rounded-[10px] p-2" />
+          <input
+            name="password"
+            type="password"
+            className="mb-[30px] rounded-[10px] p-2"
+            onChange={changeHandler}
+          />
           <button className="bg-[#112D4E] rounded-[10px] text-[#F9F7F7] p-2 hover:bg-[#3F72AF] mb-[20px] text-[20px]">
             Sign Up
           </button>
