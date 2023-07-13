a476326f11383e33f451911cd4939a4deaf779d2
fabiohidekihirose <fabiohidekihirose@gmail.com>
2023-07-13 15:32:54 +0900

    Feat: Feed page

--- src/app/home/components/Feed.tsx
+++ src/app/home/components/Feed.tsx
@@ -1,10 +1,50 @@
+import { useEffect, useState } from "react";
 import PostCard from "./../components/PostCard";
+import { FollowProps } from "../page";
+import { PostProps } from "./../components/PostCard";
+import { FavProps } from "./MyPosts";
+import axios from "axios";
+import { useAuthContext } from "@/components/AuthContext";
 
-export default function Feed() {
+interface FeedProps {
+  followingUsers: FollowProps[];
+}
+
+export default function Feed({ followingUsers }: FeedProps) {
+  const [feedPosts, setFeedPosts] = useState([]);
+  const userObj = useAuthContext();
+  const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      const followingIds = followingUsers.map(
+        (user: FollowProps) => user.user_id_ed
+      );
+      const feedPostsData = await axios.post(
+        `${baseURL}/user/${userObj.user.uid}/feed`,
+        { id_list: followingIds }
+      );
+
+      console.log(feedPostsData.data);
+      setFeedPosts(feedPostsData.data);
+    };
+
+    getFeedPosts();
+  }, []);
   return (
     <>
-      {/* {dummyData &&
-        dummyData.map((data) => <PostCard content={data.content} />)} */}
+      {feedPosts.length &&
+        feedPosts.map((feedPost: PostProps) => (
+          <PostCard
+            key={feedPost.id}
+            post={feedPost}
+            isFavorite={
+              !!feedPost.favorite.filter(
+                (favorite: FavProps) => favorite.user_id === userObj.user.uid
+              ).length
+            }
+          />
+        ))}
     </>
   );
 }
